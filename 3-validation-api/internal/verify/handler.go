a4a65759adc10201b60478c3765769192f0679d0
uginugin <uginu9in@gmail.com>
2026-02-20 01:29:04 +0500

    fix

--- 3-validation-api/internal/verify/handler.go
+++ 3-validation-api/internal/verify/handler.go
@@ -19,12 +19,13 @@ type VerifyHandlerDeps struct {
 
 type verifyHandler struct {
 	*config.Config
-	lastHash *big.Int
+	HashMail map[string]string
 }
 
 func New(router *http.ServeMux, deps *VerifyHandlerDeps) {
 	handler := &verifyHandler{
-		Config: deps.Config,
+		Config:   deps.Config,
+		HashMail: make(map[string]string),
 	}
 	router.HandleFunc("POST /send", handler.send())
 	router.Handle("GET /verify/{hash}", handler.verify())
@@ -47,12 +48,17 @@ func (h *verifyHandler) send() http.HandlerFunc {
 		e := email.NewEmail()
 		e.From = fmt.Sprintf("Jordan Wright <%s>", h.Config.Email)
 		e.To = []string{body.Email}
-		e.HTML = []byte(fmt.Sprintf("<a>http://localhost:8081/verify/%d</a>", hash))
-		e.Send(
+		e.HTML = []byte(fmt.Sprintf("<a href=\"http://localhost:8081/verify/%d\">Подтвердить email</a>", hash))
+		err = e.Send(
 			fmt.Sprintf("%s:587", h.Config.Address),
 			smtp.PlainAuth("", h.Config.Email, h.Config.Password, h.Config.Address))
 
-		h.lastHash = hash
+		if err != nil {
+			response.Json(w, 500, err.Error())
+			return
+		}
+
+		h.HashMail[hash.String()] = body.Email
 		response.Json(w, 200, hash.String())
 
 	}
@@ -61,10 +67,7 @@ func (h *verifyHandler) send() http.HandlerFunc {
 func (h *verifyHandler) verify() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		hash := r.PathValue("hash")
-		isSame := hash == h.lastHash.String()
-
-		h.lastHash = nil
-
-		response.Json(w, 200, isSame)
+		response.Json(w, 200, h.HashMail[hash] != "")
+		delete(h.HashMail, hash)
 	}
 }
